3ff758050b33a4306e9983881f3d7763f1a450a3
Runzhou Zhu <runzhouz@metropolia.fi>
2025-04-01 15:25:48 +0300

    123

---
+++ Assignmen_07_State/src/Character.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class Character {
+    private static Scanner sc = new Scanner(System.in);
+    private State state;
+    private String name;
+    private int level;
+    private int experience_points;
+    private int health_points;
+
+
+    public Character() {
+    }
+
+}
---
+++ Assignmen_07_State/src/State.java
@@ -0,0 +1,8 @@
+public abstract class State {
+    private Character character;
+
+    public State(Character character) {
+        this.character = character;
+    }
+
+}
