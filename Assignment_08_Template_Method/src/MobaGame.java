d4bea2b81a0415825672a8201773b64f203d8353
Runzhou Zhu <runzhouz@metropolia.fi>
2025-04-03 11:34:40 +0300

    123

---
+++ Assignment_08_Template_Method/src/MobaGame.java
@@ -0,0 +1,21 @@
+public class MobaGame extends Game {
+    @Override
+    public void initializeGame(int numberOfPlayers) {
+
+    }
+
+    @Override
+    public boolean endOfGame() {
+        return false;
+    }
+
+    @Override
+    public void playSingleTurn(int player) {
+
+    }
+
+    @Override
+    public void displayWinner() {
+
+    }
+}
