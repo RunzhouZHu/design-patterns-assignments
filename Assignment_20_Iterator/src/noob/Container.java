3509306f528da7f55404262b29e9bf82ced91797
Runzhou Zhu <runzhouz@metropolia.fi>
2025-05-02 15:11:55 +0300

    123

---
+++ Assignment_20_Iterator/src/noob/Container.java
@@ -0,0 +1,5 @@
+package noob;
+
+public interface Container {
+    public Iterator getIterator();
+}
---
+++ Assignment_20_Iterator/src/noob/Iterator.java
@@ -0,0 +1,6 @@
+package noob;
+
+public interface Iterator {
+    public boolean hasNext();
+    public Object next();
+}
